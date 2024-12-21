a24367e1a9a5d69cb6b768933deb9fec853d37ba
NikolayYonchev <nikolay.yonch@gmail.com>
2024-12-21 12:03:33 +0200

    Update obj Folder

--- BoardGameStore/BoardGameStore/obj/Debug/net8.0/BoardGameStore.AssemblyInfo.cs
+++ BoardGameStore/BoardGameStore/obj/Debug/net8.0/BoardGameStore.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoardGameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40fedcc8cc54bd93988d229166c5e8a45c8a0d93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f60376132a5607b794cd848ff5ed79a31aa6f3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoardGameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoardGameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
